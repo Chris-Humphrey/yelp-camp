6ef82ef1ab165f75cb69444a46b809221949b497
Chris Humphrey <clhumphrey21@gmail.com>
2019-01-09 15:35:37 +0000

    update dburl

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ var commentRoutes       = require('./routes/comments'),
     campgroundRoutes    = require('./routes/campgrounds'),
     indexRoutes          = require('./routes/index');
 
-
-// mongoose.connect("mongodb://localhost:27017/yelp_camp_v12", {useNewUrlParser: true});
-mongoose.connect("mongodb://Chris:Schlims8246@ds253094.mlab.com:53094/yelpcamp", {useNewUrlParser: true});
+// need to run in command line: DATABASEURL=mongodb://localhost:27017/yelp_camp_v12
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
+// mongoose.connect("mongodb://Chris:Schlims8246@ds253094.mlab.com:53094/yelpcamp", {useNewUrlParser: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
